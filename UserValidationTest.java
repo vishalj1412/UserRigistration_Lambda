c846750316e8477b62ea24e770ff644b95e6f671
vishalj1412 <89152210+vishalj1412@users.noreply.github.com>
2021-10-26 23:11:41 +0530

    sdff

---
+++ UserValidationTest.java
@@ -0,0 +1,67 @@
+package lambda;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.*;
+
+public class UserValidationTest {
+    /*
+    test case for first name.
+     */
+    @Test
+    public void givenFirstNameValidation() {
+        UserRegistrationValidation validation = new UserRegistrationValidation();
+try {
+    Assert.assertTrue(validation.firstNameValidation("Vishal"));
+}catch (InvalidInputException e){
+
+}
+    }
+    /*
+    test case for lst name
+     */
+    @Test
+    public void givenlastNameValidation() {
+        UserRegistrationValidation validation = new UserRegistrationValidation();
+        try {
+            Assert.assertTrue(validation.lastNameValidation("Jagtap"));
+
+        } catch (InvalidInputException e) {
+        }
+    }
+    /*
+    test case for email id.
+     */
+    @Test
+    public void givenEmailIdValidation() {
+        UserRegistrationValidation emailvalidation = new UserRegistrationValidation();
+        try {
+            Assert.assertTrue(emailvalidation.emailValidation("mkgandhi007@gmail.com"));
+        } catch (InvalidInputException e) {
+
+        }
+    }
+    /*
+    test case for mobile number.
+     */
+    @Test
+    public void givenMobileNumberValidation() {
+        UserRegistrationValidation validation = new UserRegistrationValidation();
+try{
+        Assert.assertTrue(validation.mobileNumberValidation("+918882695710"));
+    }catch (InvalidInputException e) {
+
+}
+    }
+    /*
+    test case for password
+     */
+    @Test
+    public void givenPasswordValidation() {
+        UserRegistrationValidation validation = new UserRegistrationValidation();
+        try {
+            Assert.assertTrue(validation.passwordValidation("qw1rt@Ms"));
+        }catch (InvalidInputException e) {
+
+        }
+        }
+    }
